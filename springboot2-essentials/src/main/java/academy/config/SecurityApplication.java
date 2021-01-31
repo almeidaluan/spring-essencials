e58aa15fc57e4a1d30ed237b4bfef57474771838
luan nascimento <almeida.luan099@gmail.com>
2021-01-30 23:01:26 -0300

    Sobrescrevendo usuario e senha default do spring security e adicionando usuario em memoria com respectivas roles e senha encodada

---
+++ springboot2-essentials/src/main/java/academy/config/SecurityApplication.java
@@ -0,0 +1,43 @@
+package academy.config;
+
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Log4j2
+public class SecurityApplication  extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+
+        PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        log.info("Password Encoded {}", passwordEncoder.encode("teste"));
+        log.info("Password Encoded {}", passwordEncoder.encode("teste123"));
+        auth.inMemoryAuthentication()
+                .withUser("almeidaluan")
+                .password(passwordEncoder.encode("teste123"))
+                .roles("USER","ADMIN")
+                .and()
+                .withUser("dev")
+                .password(passwordEncoder.encode("senhadev"))
+                .roles("USER");
+
+    }
+
+
+}
