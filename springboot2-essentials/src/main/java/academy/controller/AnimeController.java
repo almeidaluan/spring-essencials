f122eb348ab3ce28ba5aa76542870a1191f73338
luan nascimento <almeida.luan099@gmail.com>
2021-02-06 15:32:47 -0300

    Habilitando a Pre autorizacao para usuario que possui uma determinada role executar determinada acao

--- springboot2-essentials/src/main/java/academy/controller/AnimeController.java
+++ springboot2-essentials/src/main/java/academy/controller/AnimeController.java
@@ -12,6 +12,7 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -49,6 +50,7 @@ public class AnimeController {
     }
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Anime> SaveAnime(@RequestBody @Valid AnimeRequest animeRequest){
         return new ResponseEntity<>(animeService.SaveAnime(animeRequest), HttpStatus.CREATED);
     }
